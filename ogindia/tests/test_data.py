f8933881280ce7332f6f81ccc08e3cac7d504517
jdebacker <jason.debacker@gmail.com>
2019-08-23 23:55:17 +0530

    change test_data for india tc

--- ogindia/tests/test_data.py
+++ ogindia/tests/test_data.py
@@ -1,24 +1,23 @@
 import pytest
 import os
-from ogindia.utils import CPS_START_YEAR
 
 
-def test_cps():
-    """
-    Check that setting `data` to 'cps' uses cps data
-    """
-    from ogindia import get_micro_data
-    baseline = False
-    start_year = 2016
-    reform = {"II_em": {2017: 10000}}
-
-    calc = get_micro_data.get_calculator(
-        baseline, start_year, reform=reform,
-        records_start_year=CPS_START_YEAR, data="cps")
-    # blind_head is only in the CPS file and e00700 is only in the PUF.
-    # See taxcalc/records_variables.json
-    assert (calc.array("blind_head").sum() > 0 and
-            calc.array("e00700").sum() == 0)
+# def test_cps():
+#     """
+#     Check that setting `data` to 'cps' uses cps data
+#     """
+#     from ogindia import get_micro_data
+#     baseline = False
+#     start_year = 2016
+#     reform = {"II_em": {2017: 10000}}
+#
+#     calc = get_micro_data.get_calculator(
+#         baseline, start_year, reform=reform,
+#         records_start_year=CPS_START_YEAR, data="cps")
+#     # blind_head is only in the CPS file and e00700 is only in the PUF.
+#     # See taxcalc/records_variables.json
+#     assert (calc.array("blind_head").sum() > 0 and
+#             calc.array("e00700").sum() == 0)
 
 
 def test_set_path():
@@ -28,8 +27,8 @@ def test_set_path():
     """
     from ogindia import get_micro_data
     baseline = False
-    start_year = 2016
-    reform = {"II_em": {2017: 10000}}
+    start_year = 2017
+    reform = {2020: {'_std_deduction': [100000]}}
 
     # In theory this path doesn't exist so there should be an IOError
     # But taxcalc checks if the path exists and if it doesn't, it tries
@@ -38,34 +37,34 @@ def test_set_path():
     with pytest.raises((IOError, ValueError), match="notapath.csv"):
         get_micro_data.get_calculator(
             baseline, start_year, reform=reform,
-            records_start_year=CPS_START_YEAR, data="notapath.csv")
+            records_start_year=2017, data="notapath.csv")
 
 
-def test_puf_path():
-    """
-    Check that setting `data` to None uses the puf file
-    """
-    from ogindia import get_micro_data
-    baseline = False
-    start_year = 2016
-    reform = {"II_em": {2017: 10000}}
-
-    # get path to puf if puf.csv in ogindia/ directory
-    cur_dir = os.path.abspath(os.path.dirname(__file__))
-    puf_path = os.path.join(cur_dir, "../puf.csv")
-
-    # puf.csv in ogindia/
-    if os.path.exists(puf_path):
-        calc = get_micro_data.get_calculator(
-            baseline, start_year, reform=reform, data=puf_path)
-        # blind_head is only in the CPS file and e00700 is only in the PUF.
-        # See taxcalc/records_variables.json
-        assert (calc.array('blind_head').sum() == 0 and
-                calc.array('e00700').sum() > 0)
-    # we do not have puf.csv
-    else:
-        # make sure TC is looking for puf.csv
-        with pytest.raises((IOError, ValueError), match="puf.csv"):
-            get_micro_data.get_calculator(
-                baseline, start_year, reform=reform,
-                records_start_year=CPS_START_YEAR, data=None)
+# def test_puf_path():
+#     """
+#     Check that setting `data` to None uses the puf file
+#     """
+#     from ogindia import get_micro_data
+#     baseline = False
+#     start_year = 2016
+#     reform = {2020: {'_std_deduction': [100000]}}
+#
+#     # get path to puf if puf.csv in ogindia/ directory
+#     cur_dir = os.path.abspath(os.path.dirname(__file__))
+#     puf_path = os.path.join(cur_dir, "../puf.csv")
+#
+#     # puf.csv in ogindia/
+#     if os.path.exists(puf_path):
+#         calc = get_micro_data.get_calculator(
+#             baseline, start_year, reform=reform, data=puf_path)
+#         # blind_head is only in the CPS file and e00700 is only in the PUF.
+#         # See taxcalc/records_variables.json
+#         assert (calc.array('blind_head').sum() == 0 and
+#                 calc.array('e00700').sum() > 0)
+#     # we do not have puf.csv
+#     else:
+#         # make sure TC is looking for puf.csv
+#         with pytest.raises((IOError, ValueError), match="puf.csv"):
+#             get_micro_data.get_calculator(
+#                 baseline, start_year, reform=reform,
+#                 records_start_year=CPS_START_YEAR, data=None)
